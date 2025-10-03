b1e93b4b8726e73f84a9424c168609bd5bada412
darwin_quiroz <darquiroz@gmail.com>
2025-10-03 12:57:54 -0500

    Mensaje

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,10 @@
 onload = () => {
   document.body.classList.remove("container");
+
+  setTimeout(() => {
+    window.scrollTo({
+      top: document.body.scrollHeight, // posición final de la página
+      behavior: "smooth", // scroll animado
+    });
+  }, 8500);
 };
