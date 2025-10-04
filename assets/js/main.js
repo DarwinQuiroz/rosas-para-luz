3047a7b55bcea2f700e336c5089d9e67d285380f
Darwin Quiroz <darquiroz@gmail.com>
2025-10-04 09:18:00 -0500

    Botón para reproducir audio

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,30 @@
 onload = () => {
   document.body.classList.remove("container");
 
-  setTimeout(() => {
-    window.scrollTo({
-      top: document.body.scrollHeight, // posición final de la página
-      behavior: "smooth", // scroll animado
-    });
-  }, 8500);
+  // setTimeout(() => {
+  //   window.scrollTo({
+  //     top: document.body.scrollHeight, // posición final de la página
+  //     behavior: "smooth", // scroll animado
+  //   });
+  // }, 8500);
 };
+
+const audio = document.getElementById("musica");
+const playButton = document.getElementById("playButton");
+
+audio.play().catch(() => {
+  // Si falla, mostrar el botón
+  playButton.style.display = "block";
+});
+
+// Cuando el usuario hace clic, reproducir manualmente
+playButton.addEventListener("click", () => {
+  audio
+    .play()
+    .then(() => {
+      playButton.style.display = "none";
+    })
+    .catch((err) => {
+      alert("No se pudo reproducir el audio: " + err.message);
+    });
+});
